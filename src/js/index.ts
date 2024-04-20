2af28836b8c04447d06a8ce09c45f284b5319e31
Mario Carballo Zama <afk@ellugar.co>
2024-04-20 10:43:29 -0600

    fix: colors and add keyboard shurtcuts

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,15 +1,16 @@
 import JSON5 from "json5";
 
-import p5, { File } from "p5";
+import P5, { File } from "p5";
 import type { Element } from "p5";
+import { colors, opacity, text } from "./theme";
 
 interface Controls {
   view: { x: number; y: number; width: number; height: number; zoom: number };
   viewPos: { prevX: null | number; prevY: null | number; isDragging: boolean };
 }
 
-new p5((p5Instance) => {
-  const p5 = p5Instance as unknown as p5;
+new P5((p5Instance) => {
+  const p5 = p5Instance as unknown as P5;
 
   let steps: Step[] | undefined;
 
@@ -18,10 +19,10 @@ new p5((p5Instance) => {
 
   let slider: Element | undefined;
   let wrapper: Element | undefined;
-  let next: Element | undefined;
-  let prev: Element | undefined;
-  let nextCol: Element | undefined;
-  let prevCol: Element | undefined;
+  let buttonNext: Element | undefined;
+  let buttonPrev: Element | undefined;
+  let buttonNextCol: Element | undefined;
+  let buttonPrevCol: Element | undefined;
 
   const controls: Controls = {
     view: { x: 0, y: 0, width: 0, height: 0, zoom: 1 },
@@ -31,6 +32,23 @@ new p5((p5Instance) => {
   function v2(arr: [number, number]) {
     const [x, y] = arr;
     return p5.createVector(x, y);
+  }
+
+  function toColor(arr: number[], a: number = opacity.m) {
+    const [r, g, b] = arr;
+    return p5.color(r, g, b, a * 255);
+  }
+
+  function nextStep() {
+    if (slider == null || steps == null) return;
+    const current = Number(slider.value());
+    slider.value(Math.min(current + 1, steps.length - 1));
+  }
+
+  function prevStep() {
+    if (slider == null) return;
+    const current = Number(slider.value());
+    slider.value(Math.max(current - 1, 0));
   }
 
   function findNextCollision() {
@@ -49,10 +67,10 @@ new p5((p5Instance) => {
 
   function createControls() {
     if (slider != null) slider.remove();
-    if (next != null) next.remove();
-    if (nextCol != null) nextCol.remove();
-    if (prev != null) prev.remove();
-    if (prevCol != null) prevCol.remove();
+    if (buttonNext != null) buttonNext.remove();
+    if (buttonNextCol != null) buttonNextCol.remove();
+    if (buttonPrev != null) buttonPrev.remove();
+    if (buttonPrevCol != null) buttonPrevCol.remove();
 
     if (steps == null) return;
 
@@ -60,41 +78,37 @@ new p5((p5Instance) => {
     wrapper.addClass("wrapper");
     wrapper.position(0, 0);
 
-    prevCol = p5.createButton("<<");
-    prevCol.parent(wrapper);
+    buttonPrevCol = p5.createButton("<<");
+    buttonPrevCol.parent(wrapper);
 
-    prev = p5.createButton("<");
-    prev.parent(wrapper);
+    buttonPrev = p5.createButton("<");
+    buttonPrev.parent(wrapper);
 
     slider = p5.createSlider(0, steps.length - 1, 0);
     slider.parent(wrapper);
     const index = findNextCollision()!;
     slider.value(index);
 
-    next = p5.createButton(">");
-    next.parent(wrapper);
+    buttonNext = p5.createButton(">");
+    buttonNext.parent(wrapper);
 
-    nextCol = p5.createButton(">>");
-    nextCol.parent(wrapper);
+    buttonNextCol = p5.createButton(">>");
+    buttonNextCol.parent(wrapper);
 
-    next.mousePressed(() => {
-      if (slider == null || steps == null) return;
-      const current = Number(slider.value());
-      slider.value(Math.min(current + 1, steps.length - 1));
+    buttonNext.mousePressed(() => {
+      nextStep();
     });
 
-    prev.mousePressed(() => {
-      if (slider == null) return;
-      const current = Number(slider.value());
-      slider.value(Math.max(current - 1, 0));
+    buttonPrev.mousePressed(() => {
+      prevStep();
     });
 
-    nextCol.mousePressed(() => {
+    buttonNextCol.mousePressed(() => {
       const index = findNextCollision()!;
       slider?.value(index);
     });
 
-    prevCol.mousePressed(() => {
+    buttonPrevCol.mousePressed(() => {
       const index = findPrevCollision()!;
       slider?.value(index);
     });
@@ -118,6 +132,7 @@ new p5((p5Instance) => {
   }
 
   function setup() {
+    p5.colorMode(p5.RGB, 255);
     const width = p5.windowWidth;
     const height = p5.windowHeight - 10;
     const c = p5.createCanvas(width, height);
@@ -130,30 +145,54 @@ new p5((p5Instance) => {
     controls.view.y = height / 2 - 120;
     controls.view.zoom = 1.5;
 
+    Object.entries(colors).forEach(([key, value]) => {
+      console.log(key, value);
+      console.log(key, toColor(value));
+    });
+
     p5.strokeWeight(0.5);
 
     p5.describe("A physics debugger");
   }
 
   function draw() {
-    p5.background(100);
+    p5.background(toColor(colors.bg, 1.0));
 
     p5.translate(controls.view.x, controls.view.y);
     p5.scale(controls.view.zoom);
-    p5.rect(0, 0, 400, 240);
-    p5.fill(220);
+
+    drawScreen();
 
     if (steps) {
+      if (p5.keyIsDown(p5.SHIFT)) {
+        if (p5.keyIsDown(p5.RIGHT_ARROW)) {
+          nextStep();
+        }
+        if (p5.keyIsDown(p5.LEFT_ARROW)) {
+          prevStep();
+        }
+      }
+
       const index = Number(slider?.value());
       const step = steps[index];
 
-      p5.fill("white");
       drawStep(step);
-      drawStepName(step);
+
+      const infoHeight = 50;
+      const timelineHeight = 20;
+      const stepsInfoY = p5.windowHeight - infoHeight;
+
+      drawStepInfo(step, 0, stepsInfoY, p5.windowWidth, infoHeight);
+      drawTimeline(
+        0,
+        stepsInfoY - timelineHeight,
+        p5.windowWidth,
+        timelineHeight
+      );
     } else {
       p5.push();
-      p5.fill("black");
-      p5.textSize(20);
+      p5.fill(toColor(colors.text, opacity.l));
+      p5.textSize(text.sizeL);
       p5.textAlign(p5.CENTER, p5.CENTER);
       p5.text("Drop a physics-steps.js to start", 200, 120);
       p5.pop();
@@ -165,36 +204,55 @@ new p5((p5Instance) => {
     let end = p5.createVector(x2, y2);
 
     p5.line(start.x, start.y, end.x, end.y);
+
     p5.push();
+
     p5.noStroke();
     p5.circle(end.x, end.y, 1);
+
     p5.pop();
   }
 
-  function drawStepName(step: Step) {
-    const { name } = step;
+  function drawScreen() {
     p5.push();
+    p5.strokeWeight(2);
+    p5.stroke(colors.screenBorder);
+    p5.fill(toColor(colors.screen));
+    p5.rect(0, 0, 400, 240);
+    p5.pop();
+  }
 
+  function drawStepInfo(
+    step: Step,
+    x: number,
+    y: number,
+    width: number,
+    height: number
+  ) {
+    const { name } = step;
+
+    p5.push();
     p5.resetMatrix();
 
-    p5.fill("black");
-
-    const height = 50;
     const padding = 20;
-    const y = p5.windowHeight - 30;
 
-    p5.rect(0, p5.windowHeight - height, p5.windowWidth, height);
+    p5.fill(toColor(colors.infoBg));
+    p5.rect(x, y, p5.windowWidth, height);
 
-    p5.fill("whitw");
-    p5.textSize(20);
+    p5.fill(toColor(colors.infoFg, opacity.l));
+    p5.textSize(text.sizeM);
     p5.textAlign(p5.LEFT, p5.CENTER);
-    p5.text(name, padding, y);
+    p5.text(name, x + padding, y + padding);
 
-    p5.textAlign(p5.RIGHT, p5.CENTER);
     if (slider) {
-      p5.text(slider?.value(), p5.windowWidth - padding, y);
+      p5.textAlign(p5.RIGHT, p5.CENTER);
+      p5.text(slider?.value(), x + width - padding, y + padding);
     }
 
+    p5.pop();
+  }
+
+  function drawTimeline(x: number, y: number, width: number, height: number) {
     const minItems = 300;
     const minItemsH = minItems / 2;
 
@@ -210,42 +268,42 @@ new p5((p5Instance) => {
       count = max - min;
     }
 
-    {
-      const barHeight = 10;
-      const paddingX = 2;
-      const paddingY = 6;
-      const spacing = 1;
-      const barWidth =
-        (p5.windowWidth - paddingX * 2 - count * spacing) / count;
-      const barY = p5.windowHeight - height - barHeight - paddingY;
+    const paddingX = 2;
+    const paddingY = 6;
+    const spacing = 1;
+    const barHeight = height - paddingY * 2;
+    const barWidth = (width - paddingX * 2 - count * spacing) / count;
+    const barY = y + paddingY;
 
-      p5.fill(100);
-      p5.rect(0, barY - paddingY, p5.windowWidth, barHeight + paddingY * 2);
+    p5.push();
+    p5.resetMatrix();
 
-      for (let index = 0; index < count; index++) {
-        const step = steps[index + min];
-        p5.noStroke();
-        const hasCollision = step.collisions.length > 0;
-        const penetration = step.collisions.reduce((prev, curr) => {
-          return prev + curr.manifold.depth;
-        }, 0);
+    p5.fill(toColor(colors.timelineBg));
+    p5.rect(x, y, width, height);
 
-        if (index + min === current) {
-          p5.fill("green");
-        } else if (penetration > 6 && penetration < 3) {
-          p5.fill("red");
-        } else if (penetration > 3) {
-          p5.fill("orange");
-        } else if (hasCollision) {
-          p5.fill("yellow");
-        } else {
-          p5.fill("white");
-        }
+    for (let index = 0; index < count; index++) {
+      const step = steps[index + min];
+      p5.noStroke();
+      const hasCollision = step.collisions.length > 0;
+      const penetration = step.collisions.reduce((prev, curr) => {
+        return prev + curr.manifold.depth;
+      }, 0);
 
-        const barX = paddingX + index * (barWidth + spacing);
-
-        p5.rect(barX, barY, barWidth, barHeight);
+      if (index + min === current) {
+        p5.fill(toColor(colors.aqua));
+      } else if (penetration > 6 && penetration < 3) {
+        p5.fill(toColor(colors.warm03));
+      } else if (penetration > 3) {
+        p5.fill(toColor(colors.warm02));
+      } else if (hasCollision) {
+        p5.fill(toColor(colors.warm01));
+      } else {
+        p5.fill(toColor(colors.timelineFg));
       }
+
+      const barX = paddingX + index * (barWidth + spacing);
+
+      p5.rect(barX, barY, barWidth, barHeight);
     }
 
     p5.pop();
@@ -256,39 +314,48 @@ new p5((p5Instance) => {
     const { ball, cam_offset, collisions } = step;
     p5.translate(cam_offset[0], cam_offset[1]);
 
-    if (staticBodies) {
-      staticBodies.forEach((item) => {
+    drawStaticBodies(staticBodies);
+    drawBall(ball);
+    drawCollisions(collisions);
+
+    p5.pop();
+  }
+
+  function drawCollisions(collisions: Collision[]) {
+    collisions.forEach((item) => {
+      p5.push();
+      p5.fill(toColor(colors.collider));
+      p5.stroke(toColor(colors.collider));
+      drawCollisionShape(item.body);
+      p5.pop();
+
+      drawCollision(item);
+    });
+  }
+
+  function drawStaticBodies(bodies: Body[]) {
+    if (bodies) {
+      bodies.forEach((item) => {
         p5.push();
-        p5.fill("rgba(160,160,160,0.6)");
+        p5.fill(toColor(colors.staticBodies, opacity.staticBodies));
+        p5.noStroke();
         drawCollisionShape(item);
         p5.pop();
       });
     }
-
-    drawBall(ball);
-
-    collisions.forEach((item) => {
-      p5.push();
-      p5.fill("rgba(255,252,127,0.3)");
-      drawCollisionShape(item.body);
-      p5.pop();
-      drawCollision(item);
-    });
-    p5.pop();
   }
 
-  function drawBall(ball: Body) {
+  function drawBallInfo(ball: Body) {
     const pos = v2(ball.pos);
     const { ang_vel: angVel, ang_vel_d: angVelDel } = ball;
     const vel = v2(ball.vel).mult(10).add(pos);
     const velDelta = v2(ball.vel_d).mult(10).add(pos);
-    const posN = v2(ball.vel).add(pos);
     const shape = ball.shape as ShapeCircle;
     const r = shape.r;
 
     p5.push();
-    p5.fill("black");
-    p5.textFont("mono", 4);
+
+    p5.textSize(text.sizeS);
     p5.textAlign(p5.LEFT, p5.CENTER);
     p5.text(
       `pos: ${pos.x}, ${pos.y}
@@ -300,68 +367,110 @@ velAngDelta: ${angVelDel}
       pos.x + r + 10,
       pos.y - r
     );
-    p5.pop();
 
-    // Draw Ball
-    p5.push();
-    p5.stroke("rgba(160, 160, 255, 0.1)");
-    p5.fill("rgba(160, 160, 255, 0.1)");
-    p5.circle(pos.x, pos.y, shape.r * 2);
     p5.pop();
+  }
 
-    // Draw ball moved
-    p5.push();
-    p5.stroke("rgba(255, 160, 160, 0.4)");
-    p5.fill("rgba(255, 160, 160, 0.4)");
+  function drawBallGhost(ball: Body) {
+    const pos = v2(ball.pos);
+    const posN = v2(ball.vel).add(pos);
+    const shape = ball.shape as ShapeCircle;
+    const r = shape.r;
+
     p5.circle(posN.x, posN.y, r * 2);
-    p5.pop();
-
-    // Draw center
-    p5.push();
-    p5.noStroke();
-    p5.fill("black");
     p5.circle(posN.x, posN.y, 1);
-    p5.pop();
+  }
 
-    p5.push();
-    p5.stroke("rgba(255, 160, 160, 0.8)");
-    p5.fill("rgba(255, 160, 160, 0.8)");
+  function drawBallVel(ball: Body) {
+    const pos = v2(ball.pos);
+    const vel = v2(ball.vel).mult(10).add(pos);
     arrow(pos.x, pos.y, vel.x, vel.y);
-    p5.pop();
+  }
+
+  function drawBallVelDelta(ball: Body) {
+    const pos = v2(ball.pos);
+    const velDelta = v2(ball.vel_d).mult(10).add(pos);
 
     if (velDelta.x + velDelta.y != 0) {
-      p5.push();
-      p5.stroke("rgba(160, 160, 255, 0.5)");
-      p5.fill("rgba(160, 160, 255, 0.5)");
       arrow(pos.x, pos.y, velDelta.x, velDelta.y);
-      p5.pop();
     }
   }
 
-  function drawCollision(collision: Collision) {
+  function drawBallShape(ball: Body) {
+    const pos = v2(ball.pos);
+    const shape = ball.shape as ShapeCircle;
+    const r = shape.r;
+
+    // Draw Ball
+    p5.circle(pos.x, pos.y, r * 2);
+    p5.circle(pos.x, pos.y, 1);
+  }
+
+  function drawBall(ball: Body) {
+    p5.push();
+    p5.fill(toColor(colors.info, opacity.l));
+    p5.textSize(text.sizeS);
+    drawBallInfo(ball);
+    p5.pop();
+
+    p5.push();
+    p5.stroke(toColor(colors.ghost, opacity.s));
+    p5.fill(toColor(colors.ghost, opacity.s));
+    drawBallGhost(ball);
+    p5.pop();
+
+    p5.push();
+    p5.stroke(toColor(colors.ball));
+    p5.fill(toColor(colors.ball));
+    drawBallShape(ball);
+
+    p5.push();
+    p5.stroke(toColor(colors.velocity, opacity.l));
+    p5.fill(toColor(colors.velocity, opacity.l));
+    drawBallVel(ball);
+    p5.pop();
+
+    p5.push();
+    p5.stroke(toColor(colors.angular, opacity.l));
+    p5.fill(toColor(colors.angular, opacity.l));
+    drawBallVelDelta(ball);
+    p5.pop();
+
+    p5.pop();
+  }
+
+  function drawCollisionStart(collision: Collision) {
     const { manifold, body } = collision;
+    const a = v2(manifold.contact);
+
+    p5.circle(a.x, a.y, 1);
+  }
+
+  function drawCollisionEnd(collision: Collision) {
+    const { manifold } = collision;
     const { depth } = manifold;
-    const vel = v2(body.vel);
     const a = v2(manifold.contact);
     const b = v2(manifold.normal).normalize().mult(depth).add(a);
 
-    p5.push();
-
-    p5.noStroke();
-    p5.fill("rgba(255,160,160,.3)");
-    p5.circle(a.x, a.y, 1);
     p5.circle(b.x, b.y, 1);
-    p5.pop();
+  }
 
-    p5.push();
-    p5.stroke("rgba(255,160,160,.3)");
+  function drawCollisionDepth(collision: Collision) {
+    const { manifold } = collision;
+    const { depth } = manifold;
+
+    const a = v2(manifold.contact);
+    const b = v2(manifold.normal).normalize().mult(depth).add(a);
+
     p5.line(a.x, a.y, b.x, b.y);
-    p5.pop();
+  }
 
-    p5.push();
-    p5.fill("black");
-    p5.textFont("mono", 4);
-    p5.textAlign(p5.RIGHT, p5.CENTER);
+  function drawCollisionInfo(collision: Collision) {
+    const { manifold, body } = collision;
+    const { depth } = manifold;
+    const a = v2(manifold.contact);
+    const vel = v2(body.vel);
+
     p5.text(
       `depth: ${depth}
 vel: ${vel.x}, ${vel.y}
@@ -369,6 +478,32 @@ vel: ${vel.x}, ${vel.y}
       a.x - 20,
       a.y
     );
+  }
+
+  function drawCollision(collision: Collision) {
+    p5.push();
+    p5.fill(toColor(colors.contact01));
+    p5.noStroke();
+    drawCollisionStart(collision);
+    p5.pop();
+
+    p5.push();
+    p5.fill(toColor(colors.contact02));
+    p5.noStroke();
+    drawCollisionEnd(collision);
+    p5.pop();
+
+    p5.push();
+    p5.fill(toColor(colors.black));
+    p5.stroke(toColor(colors.black));
+    drawCollisionDepth(collision);
+    p5.pop();
+
+    p5.push();
+    p5.textAlign(p5.RIGHT, p5.CENTER);
+    p5.textSize(text.sizeS);
+    p5.fill(toColor(colors.info, opacity.l));
+    drawCollisionInfo(collision);
     p5.pop();
   }
 
@@ -378,25 +513,19 @@ vel: ${vel.x}, ${vel.y}
       case 0:
         {
           const shape = body.shape as ShapeCircle;
-          p5.push();
-          p5.noStroke();
           const p = v2(shape.p).add(v2(body.pos));
           p5.circle(p.x, p.y, shape.r * 2);
-          p5.pop();
         }
         break;
       case 2:
         {
           const shape = body.shape as ShapePolygon;
-          p5.push();
-          p5.noStroke();
           p5.beginShape();
           for (let i = 0; i < shape.verts.length; i++) {
             const a = v2(shape.verts[i]);
             p5.vertex(a.x, a.y);
           }
           p5.endShape(p5.CLOSE);
-          p5.pop();
         }
         break;
       case 3:
@@ -406,14 +535,10 @@ vel: ${vel.x}, ${vel.y}
           const b = v2(shape.b);
           const { ra, rb } = shape;
 
-          p5.push();
-          p5.noStroke();
-
           p5.circle(a.x, a.y, ra * 2);
           p5.circle(b.x, b.y, rb * 2);
 
-          p5.stroke("rgba(160,160,160,0.6)");
-          // line(a.x, a.y, b.x, b.y);
+          p5.line(a.x, a.y, b.x, b.y);
 
           const theta = Math.atan2(b.y - a.y, b.x - a.x);
           const rot = {
@@ -429,13 +554,20 @@ vel: ${vel.x}, ${vel.y}
 
           p5.line(tan_1_a.x, tan_1_a.y, tan_1_b.x, tan_1_b.y);
           p5.line(tan_2_a.x, tan_2_a.y, tan_2_b.x, tan_2_b.y);
-
-          p5.pop();
         }
         break;
       default:
         console.warn("Shape type not handled", shapeType);
         break;
+    }
+  }
+
+  function keyPressed() {
+    const { keyCode } = p5;
+    if (keyCode === p5.LEFT_ARROW) {
+      prevStep();
+    } else if (keyCode === p5.RIGHT_ARROW) {
+      nextStep();
     }
   }
 
@@ -509,6 +641,7 @@ vel: ${vel.x}, ${vel.y}
 
   p5.setup = setup;
   p5.draw = draw;
+  p5.keyPressed = keyPressed;
   p5.mouseWheel = mouseWheel;
   p5.mouseDragged = mouseDragged;
   p5.mousePressed = mousePressed;
---
+++ src/js/theme.ts
@@ -0,0 +1,75 @@
+const opacityS = 0.6;
+const opacityM = 0.8;
+const opacityL = 1.0;
+
+export const opacity = {
+  s: opacityS,
+  m: opacityM,
+  l: opacityL,
+
+  staticBodies: opacityS,
+};
+
+export const stroke = {
+  opacity: 0.1,
+  weight: 1,
+};
+
+const white = [255, 255, 255];
+const fullBlack = [0, 0, 0];
+const black = [65, 67, 69];
+const yellow = [255, 242, 76];
+const orange = [255, 119, 76];
+const aqua = [71, 255, 196];
+const carmin = [255, 71, 93];
+const blue = [116, 164, 183];
+const violet = [174, 80, 255];
+const lila = [176, 180, 233];
+const gray = [180, 180, 180];
+
+export const text = {
+  sizeS: 10,
+  sizeM: 20,
+  sizeL: 25,
+};
+
+export const colors = {
+  black,
+  yellow,
+  orange,
+  gray,
+  aqua,
+  carmin,
+  blue,
+  violet,
+  lila,
+  white,
+
+  bg: [65, 65, 65],
+  screen: white,
+  screenBorder: fullBlack,
+  text: fullBlack,
+
+  cool: aqua,
+  warm01: yellow,
+  warm02: orange,
+  warm03: carmin,
+
+  infoBg: fullBlack,
+  infoFg: white,
+
+  timelineBg: fullBlack,
+  timelineFg: white,
+
+  info: black,
+  staticBodies: gray,
+  flippers: blue,
+  penetration: black,
+  ball: carmin,
+  ghost: aqua,
+  contact01: yellow,
+  contact02: orange,
+  velocity: carmin,
+  angular: violet,
+  collider: lila,
+};
